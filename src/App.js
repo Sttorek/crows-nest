ed698b70e96a4067cc8c93fc3e800e83f17a39b9
Seth Torek <seth.torek@gmail.com>
2021-06-06 19:24:20 -0400

    created navbar component and added background color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-     
+       <style>{'body { background-color: #141414; }'}</style>
+     <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import logo from "./logo.png";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1 className='crowsNest'>Crows <br/> Nest</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
