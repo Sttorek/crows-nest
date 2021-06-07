d28097eae54e30ed4151169f40e11499ac9827c2
Seth Torek <seth.torek@gmail.com>
2021-06-07 01:00:31 -0400

    styles homepage added image

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,61 +1,62 @@
 import React from "react";
+import towerImg from './towerImg.jpg'
 
 function Homepage() {
   return (
-    <section>
+    <section className='homeCnt'>
       <div className="signUpCnt">
         <p className='signUpTitle'>Sign Up</p>
 
-        <div class="input-group input-group-sm mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-sm">
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
             Company ID
           </span>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             aria-label="Sizing example input"
             aria-describedby="inputGroup-sizing-sm"
           />
         </div>
 
-        <div class="input-group input-group-sm mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-sm">
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
             First Name
           </span>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             aria-label="Sizing example input"
             aria-describedby="inputGroup-sizing-sm"
           />
         </div>
 
-        <div class="input-group input-group-sm mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-sm">
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
             Last Name
           </span>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             aria-label="Sizing example input"
             aria-describedby="inputGroup-sizing-sm"
           />
         </div>
 
-        <div class="input-group input-group-sm mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-sm">
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
             Email
           </span>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             aria-label="Sizing example input"
             aria-describedby="inputGroup-sizing-sm"
           />
         </div>
 
-        <div class="input-group input-group-sm mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-sm">
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
             Password
           </span>
           <input
@@ -66,15 +67,20 @@ function Homepage() {
           />
         </div>
 
-        <select class="form-select" aria-label="Default select example">
+        <select className="form-select" aria-label="Default select example">
           <option selected>Role</option>
           <option value="1">Management</option>
           <option value="2">Employee</option>
         </select>
 
-        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" className="btn btn-primary signUpBtn">Submit</button>
       </div>
-      <div></div>
+
+      {/* serperate----------------------------------- */}
+
+      <div className='homeImgCnt'>
+        <img className='homeImg' src={ towerImg }/>
+      </div>
     </section>
   );
 }
